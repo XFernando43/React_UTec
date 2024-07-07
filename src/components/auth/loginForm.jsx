30bde19f3cebbd8b6c706547bb4fff242c8e2cb1
XFernando43 <U20171E997@upc.edu.pe>
2024-07-07 15:14:56 -0500

    add: function

--- src/components/auth/loginForm.jsx
+++ src/components/auth/loginForm.jsx
@@ -1,48 +1,71 @@
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { Image } from "primereact/image";
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { Image } from 'primereact/image';
-import React from 'react';
-
-export default function LoginForm(){
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+export default function LoginForm() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
     const header = (
-        // <img alt="user header" src="../../assets/329182497_580658887303089_6817430973110470755_n.jpg" className="pointer-events-none" />
-        <Image src="https://static-cdn.jtvnw.net/jtv_user_pictures/b661c2ac-7563-4815-8762-0f0a8686c87e-profile_image-300x300.png" alt="Image" width="250" className='pointer-events-none'/>
-        // <Image src="../../assets/react.svg" alt="Image" width="250" className='pointer-events-none' />
-
-        // <h1>PIPIPI</h1>
+        <Image
+            src="https://utec.edu.pe/sites/all/themes/utec_theme/assets/landings/integra-utec/img/logo-utec-negro-h.svg"
+            className="pointer-events-none"
+        />
     );
 
     const footer = (
         <div className="flex gap-3 mt-1">
-            <Button label="Register" severity="warning"  outlined className="w-full hover:bg-red-600 hover:text-white" onClick={() => redirectToRoute()} />
-            <Button label="Login" className="w-full" />
+            <Link to="/home" className="w-full">
+                <Button
+                    label="Register"
+                    severity="warning"
+                    outlined
+                    className="w-full hover:bg-red-600 hover:text-white"
+                />
+            </Link>
+
+            <Link to="/home" className="w-full">
+                <Button label="Login" className="w-full" />
+            </Link>
         </div>
     );
 
-    const redirectToRoute = () => {
-        // Add your routing logic here
-    };
 
 
-    return(
-        // <div className="card flex justify-content-center">
-            <Card title="IDBI Login" subTitle="Ingrese su usuario" footer={footer} header={header} className="max-w-lg select-none rounded-xl">
-                <div className="flex flex-col gap-4">
-                    <div className="flex flex-col gap-2">
-                        <label htmlFor="email">Email</label>
-                        <InputText value={email} onChange={(e) => setEmail(e.target.value)} type="mail" id="email" aria-describedby="Email" />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <label htmlFor="password">Password</label>
-                        <InputText value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" aria-describedby="password" />
-                    </div>
+    return (
+        <Card
+            title="IDBI Login"
+            subTitle="Logese para poder ingresar"
+            footer={footer}
+            header={header}
+            className="max-w-lg select-none rounded-xl"
+        >
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="email">Email</label>
+                    <InputText
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="mail"
+                        id="email"
+                        aria-describedby="Email"
+                    />
                 </div>
-            </Card>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="password">Password</label>
+                    <InputText
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        id="password"
+                        aria-describedby="password"
+                    />
+                </div>
+            </div>
+        </Card>
         // </div>
-    )
-  }
+    );
+}
